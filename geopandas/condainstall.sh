fc760339f7d587f320190d1ce4e608f135bf4e07
Landung Setiawan <landungs@uw.edu>
2017-01-26 13:27:06 -0800

    Delete condainstall.sh

--- geopandas/condainstall.sh
+++
@@ -1,7 +0,0 @@
-#!bin/bash
-
-CONDAURL=https://repo.continuum.io/miniconda/Miniconda2-latest-Linux-x86_64.sh
-curl $CONDAURL -o miniconda.sh
-bash miniconda.sh -b
-export PATH=$HOME/miniconda2/bin:$PATH
-conda update --yes --all
